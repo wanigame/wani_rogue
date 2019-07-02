19e6f8384fa75279ef2e78246199aceda6233fb8
wanigame <wanigame82@gmail.com>
2019-07-02 23:43:01 +0900

    Add Painter class in JS

---
+++ app/scripts/painter.js
@@ -0,0 +1,25 @@
+// Painter for canvas.
+//
+// Copyright (c) 2019 wanigame
+//
+// This source code is released under the MIT License
+// http://opensource.org/licenses/mit-license.php
+
+export class Painter {
+    context;
+
+    constructor() {
+        this.context = document.getElementById("main").getContext("2d");
+    }
+
+    drawRect(x, y, width, height, color) {
+        this.context.fillStyle = color;
+        this.context.fillRect(x, y, width, height);
+
+        console.log(color);
+    }
+
+    color(r, g, b, a = 0xff) {
+        return "rgba(" + r + "," + g + "," + b + "," + a / 0xff + ")";
+    }
+}
