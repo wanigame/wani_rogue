698b11294f957d5b6d23a0ac1e2e7fa56d60b42f
wanigame <wanigame82@gmail.com>
2019-07-03 23:37:42 +0900

    Make entry a class

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -5,27 +5,34 @@
 // This source code is released under the MIT License
 // http://opensource.org/licenses/mit-license.php
 
-let _exports;
-let _log = "";
 
-fetch("wani_rogue.wasm")
-    .then((response) => response.arrayBuffer())
-    .then((bytes) => WebAssembly.instantiate(bytes, imports))
-    .then((results) => {
-        _exports = results.instance.exports;
-        _exports.init();
-    });
+class Entry {
+    exports;
+    log = "";
 
-const imports = {
-    env: {
-        js_log: function (log) {
-            let c = String.fromCharCode(log);
-            if (c == "\n") {
-                console.log(_log);
-                _log = "";
-            }
-            else _log = _log + c;
-        },
-        js_random: function (max) { return Math.floor(Math.random() * max); },
+    imports = {
+        env: {
+            js_log: function (log) {
+                let c = String.fromCharCode(log);
+                if (c == "\n") {
+                    console.log(this.log);
+                    this.log = "";
+                }
+                else this.log += c;
+            }.bind(this),
+            js_random: function (max) { return Math.floor(Math.random() * max); },
+        }
+    }
+
+    constructor() {
+        fetch("wani_rogue.wasm")
+            .then((response) => response.arrayBuffer())
+            .then((bytes) => WebAssembly.instantiate(bytes, this.imports))
+            .then((results) => {
+                this.exports = results.instance.exports;
+                this.exports.init();
+            });
     }
 }
+
+new Entry();
